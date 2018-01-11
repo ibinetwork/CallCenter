1a27eff65a1a1af59922859044b80aae727b37c2
Rodion Nosankov <geforce.exe@gmail.com>
2018-01-11 11:23:08 +0200

    monitoring last number

--- Scripts/asterisk_missing_call.sh
+++ Scripts/asterisk_missing_call.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-log="/var/log/nginx/access.log"
+log="queue_log"
+queue=1192
 x=0
 total=0
 while true; do
-				sleep 1
-				let x=$x+1
-				cmd=$(cat $log | grep -E "403" | awk '{print $1}' | wc -l)
-				if [ $? == 0 ]; then
-								let total=$total+1
-				else
-								echo Error
-				fi
-				if [ $x -gt 10 ]; then
-								total=0
-				else	
-								echo "Counter: " $total
-				fi
+	sleep 1
+	cmd=$(cat $log | grep $queue)
+	if [ $? == 0 ]; then
+		echo "${cmd}" | grep ENTERQUEUE | sed 's\|\ \g' | awk '{print $6}' | tail -n1
+	else 
+		echo ERROR
+	fi
 done
